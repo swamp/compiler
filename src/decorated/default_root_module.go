1922250fbabed7f865b565c3fd5c69dbc77e115e
Peter Bjorklund <piot@hotmail.com>
2021-10-25 18:34:17 +0200

    feat: add Blob.drawWindow2d!

--- src/decorated/default_root_module.go
+++ src/decorated/default_root_module.go
@@ -185,6 +185,7 @@ __externalfn toString2d : { width : Int, height : Int } -> Blob -> String
 __externalfn get2d : { x : Int, y : Int } -> { width : Int, height : Int } -> Blob -> Maybe Int
 __externalfn slice2d : { x : Int, y : Int } -> { width : Int, height : Int } -> { width : Int, height : Int } -> Blob -> Blob
 __externalfn fill2d! : { x : Int, y : Int } -> { width : Int, height : Int } -> Int -> { width : Int, height : Int } -> Blob -> Blob
+__externalfn drawWindow2d! : { x : Int, y : Int } -> { width : Int, height : Int } -> { width : Int, height : Int } -> Blob -> Blob
 __externalfn member : Int -> Blob -> Bool
 __externalfn any : (Int -> Bool) -> Blob -> Bool
 __externalfn fromArray : Array Int -> Blob
